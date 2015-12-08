6639f8e5ad03dc6963ca531f8120e800406e6f5c
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-08 16:30:27 +0100

    added fk(user) to resource and review models

--- studypy/resources/models.py
+++ studypy/resources/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 
 class Resource(models.Model):
     name = models.CharField('name', unique=True)
     url = models.URLField('resource url', blank=True)
     description = models.CharField('description')
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL)
 
 
 class Review(models.Model):
     resource = models.ForeignKey('Resource', verbose_name='resource')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='author')
