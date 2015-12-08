572bba656112243ff3119f08547e15b6f4347323
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-08 16:35:52 +0100

    added max_length attrs, added contents to review

--- studypy/resources/models.py
+++ studypy/resources/models.py
@@ -3,12 +3,13 @@ from django.conf import settings
 
 
 class Resource(models.Model):
-    name = models.CharField('name', unique=True)
+    name = models.CharField('name', unique=True, max_length=100)
     url = models.URLField('resource url', blank=True)
-    description = models.CharField('description')
+    description = models.CharField('description', max_length=2000)
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL)
 
 
 class Review(models.Model):
     resource = models.ForeignKey('Resource', verbose_name='resource')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='author')
+    contents = models.TextField('contents')
