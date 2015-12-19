02105218809bcdedcbca889ce3a4d065f30f1aa3
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-19 22:13:28 +0100

    messages in reviews

--- studypy/reviews/views.py
+++ studypy/reviews/views.py
@@ -1,17 +1,20 @@
-from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.http import Http404
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 from .forms import UpdateReviewForm, ReviewForm
 from resources.models import Resource, Review
 
 
-class UpdateReview(UpdateView):
+class UpdateReview(SuccessMessageMixin, UpdateView):
     model = Review
     form_class = UpdateReviewForm
     template_name = 'reviews/update_review.html'
     context_object_name = 'form'
     success_url = reverse_lazy('user_reviews')
+    success_message = 'Review updated successfully'
 
     def get(self, request, *args, **kwargs):
         review_pk = kwargs['pk']
@@ -22,10 +25,11 @@ class UpdateReview(UpdateView):
         return super(UpdateReview, self).get(request, *args, **kwargs)
 
 
-class AddReview(CreateView):
+class AddReview(SuccessMessageMixin, CreateView):
     model = Review
     template_name = 'reviews/add_review.html'
     form_class = ReviewForm
+    success_message = 'Review added successfully'
 
     def get_form_kwargs(self):
         kwargs = super(AddReview, self).get_form_kwargs()
