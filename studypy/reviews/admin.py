7d5ab2e627245f95bdbe640eda823243b7b3c48b
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-25 23:32:48 +0100

    reviews search, colums(admin)

--- studypy/reviews/admin.py
+++ studypy/reviews/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Review)
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('resource', 'author', 'created_at', 'modified_at')
+    search_fields = ('contents', )
