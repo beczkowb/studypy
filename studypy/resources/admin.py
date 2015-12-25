fc50be1dba38055de790fa505b552d70240a1be0
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-25 23:29:00 +0100

    resources admin search, list columns

--- studypy/resources/admin.py
+++ studypy/resources/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Resource)
+@admin.register(models.Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('url', 'added_by', 'created_at', 'modified_at')
+    search_fields = ['description', 'name', 'url']
