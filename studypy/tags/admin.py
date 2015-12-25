e8eb96570c2c8f9bc936d9ac555c52ac44f03461
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2015-12-25 23:39:07 +0100

    tags search, columns

--- studypy/tags/admin.py
+++ studypy/tags/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Tag)
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name',]
