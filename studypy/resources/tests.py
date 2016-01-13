6a9e41d7b25e2bb136e1e78c11c4971b55a125fe
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2016-01-13 14:54:38 +0100

    changed admin

---
+++ studypy/resources/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_hot(self):
+        pass
--- studypy/users/admin.py
+++ studypy/users/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    fields = ('username', 'password', 'is_superuser', 'first_name', 'last_name', 'email', 'image')
     list_display = ('username', 'first_name', 'last_name', 'email')
     search_fields = ['username', 'first_name', 'last_name', 'email']
+
+admin.site.unregister(Group)
